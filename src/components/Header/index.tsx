497f765222c12d85bc22509f14a5a07fd1c08137
Deivith <deivithfagundes@gmail.com>
2026-03-12 11:47:06 -0300

    fetch: HTML header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
 export const Header = () => {
   return (
     <div>
-      <h1></h1>
+      <h1>SchedAi</h1>
+      <ul>
+        <li>Features</li>
+        <li>AI</li>
+        <li>Pricing</li>
+        <li>Testimonials</li>
+      </ul>
+      <button>Free Trial</button>
     </div>
   )
 }
