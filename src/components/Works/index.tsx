b97f17e62adc3ba7eae13d8d88c7a9ff56e84418
Deivith <deivithfagundes@gmail.com>
2026-03-14 18:35:16 -0300

    fetch: HTML Works

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -2,7 +2,36 @@ export const Works = () => (
   <div>
     <section className="container">
       <span>How it Works</span>
-      <h2>SchedAI uses artificial intelligence to organize your schedule and eliminate the hassle of manual booking.</h2>
+      <h2>
+        SchedAI uses artificial intelligence to organize your schedule and
+        eliminate the hassle of manual booking.
+      </h2>
+      <ul>
+        <li>
+          <div>
+            <img src="" />
+            <span>1</span>
+          </div>
+          <h3></h3>
+          <p></p>
+        </li>
+        <li>
+          <div>
+            <img src="" />
+            <span>1</span>
+          </div>
+          <h3></h3>
+          <p></p>
+        </li>
+        <li>
+          <div>
+            <img src="" />
+            <span>1</span>
+          </div>
+          <h3></h3>
+          <p></p>
+        </li>
+      </ul>
     </section>
   </div>
 )
